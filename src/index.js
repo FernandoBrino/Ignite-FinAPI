d91269549f12e92f0322189408dc2578940a105b
Fernando Brino <fernandobrino11@gmail.com>
2022-03-09 13:43:29 -0300

    create an account route, without DB

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { v4:uuidv4 } = require("uuid"); // v4 generate a random number
+
+const app = express();
+
+const customers = [];
+
+app.use(express.json());
+
+// cpf - string
+// name - string
+// id - uuid - using uuid package
+// statement -[]
+
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        return response.status(400).json({error: "Customer already exists!"});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(), 
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.listen(3333);
